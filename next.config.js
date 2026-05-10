908eab6e273910a0c403138587a7e5fddd72ec9f
“masum182” <masum.mollah@webable.digital>
2026-05-10 19:39:53 +0600

    asdsad

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'nextjs13-app-omega.vercel.app',
+          },
+        ],
+        destination: 'https://sn-agro-farm.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
